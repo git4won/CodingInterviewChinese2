34542d2f2afc224a50abe62eaeaffcc050e1ed63
git4won <846820279@qq.com>
2018-07-03 00:28:53 +0800

    add overload function, remove unused files and add a Makefile

--- 01_AssignmentOperator/AssignmentOperator.cpp
+++ 01_AssignmentOperator/AssignmentOperator.cpp
@@ -15,6 +15,16 @@ https://github.com/zhedahht/CodingInterviewChinese2/blob/master/LICENSE.txt)
 // 面试题1：赋值运算符函数
 // 题目：如下为类型CMyString的声明，请为该类型添加赋值运算符函数。
 
+/* 修改
+ *
+ * Date: 20180703
+ * 1. 对常量字符串增加 const 修饰：const char *text = "Hello world"; 
+ * 2. 增加 CMyString(char* pData = nullptr) 的重载函数 CMyString(const char* pData)
+ *
+ * by won
+ */
+
+
 #include<cstring>
 #include<cstdio>
 
@@ -22,6 +32,7 @@ class CMyString
 {
 public:
     CMyString(char* pData = nullptr);
+    CMyString(const char* pData);
     CMyString(const CMyString& str);
     ~CMyString(void);
 
@@ -34,6 +45,22 @@ private:
 };
 
 CMyString::CMyString(char *pData)
+{
+    if(pData == nullptr)
+    {
+        m_pData = new char[1];
+        m_pData[0] = '\0';
+    }
+    else
+    {
+        int length = strlen(pData);
+        m_pData = new char[length + 1];
+        strcpy(m_pData, pData);
+    }
+}
+
+// 重载
+CMyString::CMyString(const char *pData)
 {
     if(pData == nullptr)
     {
@@ -84,10 +111,10 @@ void Test1()
 {
     printf("Test1 begins:\n");
 
-    char* text = "Hello world";
+    const char *text = "Hello world";
 
-    CMyString str1(text);
-    CMyString str2;
+    CMyString str1(text);//调用 CMyString::CMyString(const char *pData);
+    CMyString str2;      //调用 CMyString::CMyString(char *pData);
     str2 = str1;
 
     printf("The expected result is: %s.\n", text);
@@ -102,7 +129,7 @@ void Test2()
 {
     printf("Test2 begins:\n");
 
-    char* text = "Hello world";
+    const char *text = "Hello world";
 
     CMyString str1(text);
     str1 = str1;
@@ -119,7 +146,7 @@ void Test3()
 {
     printf("Test3 begins:\n");
 
-    char* text = "Hello world";
+    const char *text = "Hello world";
 
     CMyString str1(text);
     CMyString str2, str3;
@@ -138,11 +165,29 @@ void Test3()
     printf(".\n");
 }
 
+void Test4()
+{
+    printf("Test4 begins:\n");
+
+    const char *text1 = "Hello world";
+    char text2[20] = "Hello world";
+
+    CMyString str1(text1); //调用 CMyString::CMyString(const char *pData);
+    CMyString str2(text2); //调用 CMyString::CMyString(char *pData);
+
+    printf("The actual result is: ");
+    str1.Print();
+    printf(".\n");
+    str2.Print();
+    printf(".\n");
+}
+
 int main(int argc, char* argv[])
 {
     Test1();
     Test2();
     Test3();
+    Test4();
 
     return 0;
 }
